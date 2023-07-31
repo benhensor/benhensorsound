8f619a5934e811b7882c465b578b6dc3b0d91f81
Ben Hensor <benhensor@gmail.com>
2023-07-31 12:01:03 +0100

    logo and favicon changed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Logo from '../../assets/img/logo2023.png';
+import Logo from '../../assets/img/bhsoundLogo.svg';
 import './header.css';
 
 
